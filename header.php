353211c1bf6eeb5ebca955990123c10ed72ff50c
Limekiller <me@bryceyoder.com>
2020-04-02 01:20:44 -0400

    Implement scrolling images

--- header.php
+++ header.php
@@ -6,9 +6,10 @@
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <meta name="description" content="">
-  <meta name="author" content="">
+  <meta name="author" content="Bryce Yoder">
 
   <title><?php echo get_bloginfo( 'name' ); ?></title>
+  <script src="https://kit.fontawesome.com/cae1618de2.js" crossorigin="anonymous"></script>
   <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css" rel="stylesheet">
   <link rel="stylesheet" href="https://use.typekit.net/kok3gzl.css">
   <link href="<?php echo get_bloginfo( 'template_directory' );?>/style.css" rel="stylesheet">
@@ -21,23 +22,32 @@
 </head>
 
 <body>
-
-  <div class="container">
-
     <div class="blog-header">
         <div class='header-content'>
             <a class="logo" href="<?php echo get_bloginfo('wpurl');?>"><img src='http://localhost/goshen_historical/wp-content/uploads/2020/04/logo.png'/></a>
             <img class='divider' src='http://localhost/goshen_historical/wp-content/uploads/2020/04/divider.png' />
             <nav class="blog-nav">
-                <a class="blog-nav-item active" href="#">Home</a>
                 <?php wp_list_pages( '&title_li=' ); ?>
             </nav>
             <img class='divider' src='http://localhost/goshen_historical/wp-content/uploads/2020/04/divider.png' />
-            <div class='address'>
+            <div class='info'>
                 <span>The Adam's Store</span><br />
                 <span>124 S. Main St</span><br />
-                <span>Goshen, IN 46526</span><br />
+                <span>Goshen, IN 46526</span><br /><br />
+                <span>(574) 975-0033</span>
+                <a href='mailto:museum@goshenhistorical.org'><i>museum@goshenhistorical.org</i></a>
             </div>
+            <div class='social-container'>
+                <a href='https://facebook.com'><i class="fab fa-facebook-square"></i></a>
+                <a href='https://instagram.com'><i class="fab fa-instagram"></i></a>
+            </div>
+            <img class='divider' src='http://localhost/goshen_historical/wp-content/uploads/2020/04/divider.png' />
+            <b>Hours</b><br /><br />
+            <span>Thur, Fri, Sat</span><br />
+            <span><i>11:00 AM – 3:00 PM</i></span><br /><br />
+            <span>First Friday</span><br />
+            <span><i>3:00PM – 8:00PM</i></span><br /><br />
+            <span><i>Other times by appointment</i></span>
         </div>
         <img class='header-background' src="http://localhost/goshen_historical/wp-content/uploads/2020/04/paper_tear.png">
     </div>
--- index.php
+++ index.php
@@ -1,8 +1,31 @@
 <?php get_header(); ?>
-    <div class="row">
-        <div class="col-sm-8 blog-main">
-            <?php get_template_part( 'content', get_post_format() ); ?>
-        </div> <!-- /.blog-main -->
-        <?php get_sidebar(); ?>
-    </div> <!-- /.row -->
+    <div class='scrolling-images'>
+        <div class='scrolling-images-container first'>
+            <div class='img'></div>
+            <div class='img'></div>
+            <div class='img'></div>
+            <div class='img'></div>
+            <div class='img'></div>
+            <div class='img'></div>
+        </div>
+        <div class='scrolling-images-container second'>
+            <div class='img'></div>
+            <div class='img'></div>
+            <div class='img'></div>
+            <div class='img'></div>
+            <div class='img'></div>
+            <div class='img'></div>
+        </div>
+    </div>
+    <div class="container">
+        <div class='title'>
+            <h1>Step into history</h1>
+            <h2><i>Right here in Goshen</i></h2>
+        </div>
+        <div class="row">
+            <div class="col-sm-8 blog-main">
+                <?php get_template_part( 'content', get_post_format() ); ?>
+            </div> <!-- /.blog-main -->
+            <?php get_sidebar(); ?>
+        </div> <!-- /.row -->
 <?php get_footer(); ?>
