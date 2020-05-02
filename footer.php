f149341d3e3f9479cc7697f458b55879f203ae23
Limekiller <me@bryceyoder.com>
2020-05-02 00:26:29 -0400

    Alter image file paths

--- footer.php
+++ footer.php
@@ -1,16 +1,17 @@
 </div> <!-- /.container -->
 
-    <footer class="blog-footer">
-        <div class='container right'>
-            <div class='row'> 
-                <div class='col-sm-12'>
-                    <span>Copyright <?php echo date("Y");?></span><br />
-                    <span>Designed by <a target='_blank' href='https://bryceyoder.com'>Bryce Yoder</a></span>
-                </div>
-           </div>
-        </div>
-    </footer>
+        <footer class="blog-footer">
+            <div class='container right'>
+                <div class='row'> 
+                    <div class='col-sm-12'>
+                        <span>Copyright <?php echo date("Y");?></span><br />
+                        <span>Designed by <a target='_blank' href='https://bryceyoder.com'>Bryce Yoder</a></span>
+                    </div>
+               </div>
+            </div>
+        </footer>
 
-  <?php wp_footer(); ?>
+      <?php wp_footer(); ?>
+    </div> <!-- /.site-wrapper -->
   </body>
 </html>
--- header.php
+++ header.php
@@ -4,7 +4,7 @@
 <head>
   <meta charset="utf-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1">
   <meta name="description" content="">
   <meta name="author" content="Bryce Yoder">
 
@@ -20,33 +20,34 @@
 </head>
 
 <body>
-    <div class="blog-header">
-        <div class='header-content'>
-            <a class="logo" href="<?php echo get_bloginfo('wpurl');?>"><img src='http://localhost/goshen_historical/wp-content/uploads/2020/04/logo.png'/></a>
-            <img class='divider' src='http://localhost/goshen_historical/wp-content/uploads/2020/04/divider.png' />
-            <nav class="blog-nav">
-                <?php wp_nav_menu(array('theme_location' => 'header-menu')); ?>
-            </nav>
-            <img class='divider' src='http://localhost/goshen_historical/wp-content/uploads/2020/04/divider.png' />
-            <div class='info'>
-                <span>The Adam's Store</span><br />
-                <span>124 S. Main St</span><br />
-                <span>Goshen, IN 46526</span><br /><br />
-                <span>(574) 975-0033</span>
-                <a href='mailto:museum@goshenhistorical.org'><i>museum@goshenhistorical.org</i></a>
+    <div class='site-wrapper'>
+        <div class="blog-header">
+            <div class='header-content'>
+                <a class="logo" href="<?php echo get_bloginfo('wpurl');?>"><img src='http://xbp.qvj.mybluehost.me/Devl_GHS/wp-content/uploads/2020/04/logo.png'/></a>
+                <img class='divider' src='http://xbp.qvj.mybluehost.me/Devl_GHS/wp-content/uploads/2020/04/divider.png' />
+                <nav class="blog-nav">
+                    <?php wp_nav_menu(array('theme_location' => 'header-menu')); ?>
+                </nav>
+                <img class='divider' src='http://xbp.qvj.mybluehost.me/Devl_GHS/wp-content/uploads/2020/04/divider.png' />
+                <div class='info'>
+                    <span>The Adam's Store</span><br />
+                    <span>124 S. Main St</span><br />
+                    <span>Goshen, IN 46526</span><br /><br />
+                    <span>(574) 975-0033</span>
+                    <a href='mailto:museum@goshenhistorical.org'><i>museum@goshenhistorical.org</i></a>
+                </div>
+                <div class='social-container'>
+                    <a href='https://facebook.com'><i class="fab fa-facebook-square"></i></a>
+                    <a href='https://instagram.com'><i class="fab fa-instagram"></i></a>
+                </div>
+                <img class='divider' src='http://xbp.qvj.mybluehost.me/Devl_GHS/wp-content/uploads/2020/04/divider.png' />
+                <br /><b>Hours</b><br /><br />
+                <span>Thur, Fri, Sat</span><br />
+                <span><i>11:00 AM – 3:00 PM</i></span><br /><br />
+                <span>First Friday</span><br />
+                <span><i>3:00PM – 8:00PM</i></span><br /><br />
+                <span><i>Other times by appointment</i></span>
             </div>
-            <div class='social-container'>
-                <a href='https://facebook.com'><i class="fab fa-facebook-square"></i></a>
-                <a href='https://instagram.com'><i class="fab fa-instagram"></i></a>
-            </div>
-            <img class='divider' src='http://localhost/goshen_historical/wp-content/uploads/2020/04/divider.png' />
-            <br /><b>Hours</b><br /><br />
-            <span>Thur, Fri, Sat</span><br />
-            <span><i>11:00 AM – 3:00 PM</i></span><br /><br />
-            <span>First Friday</span><br />
-            <span><i>3:00PM – 8:00PM</i></span><br /><br />
-            <span><i>Other times by appointment</i></span>
+            <img class='header-background' src="http://xbp.qvj.mybluehost.me/Devl_GHS/wp-content/uploads/2020/04/paper_tear.png"/>
+            <img class='header-hand' src='http://xbp.qvj.mybluehost.me/Devl_GHS/wp-content/uploads/2020/04/hand.png' />
         </div>
-        <img class='header-background' src="http://localhost/goshen_historical/wp-content/uploads/2020/04/paper_tear.png"/>
-        <img class='header-hand' src='http://localhost/goshen_historical/wp-content/uploads/2020/04/hand.png' />
-    </div>
