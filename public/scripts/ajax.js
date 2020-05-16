e3bb8e48289e613e5986a5038869f28210e3cf02
Limekiller <me@bryceyoder.com>
2020-05-16 19:11:20 -0400

    Make loading-speed-agnostic

--- public/scripts/ajax.js
+++ public/scripts/ajax.js
@@ -1,19 +1,26 @@
 jQuery(document).ready(function() {
-    window.setTimeout(() => {
-        jQuery('.add_to_cart_button').click(() => {
-            window.setTimeout(() => {
-                jQuery.post({
-                    url:'../wp-admin/admin-ajax.php',
-                    datatype: 'JSON',
-                    data: {action:'get_wc_cart_info'},
-                    success:function(result) {
-                        let parsedResult = JSON.parse(result);
-                        jQuery('.custom-cart').removeClass('hidden');
-                        jQuery('.items').html(parsedResult[0]);
-                        jQuery('.price').html(parsedResult[1]);
-                    }
-                });
-            }, 1000);
-        });
-    }, 1000);
+    var checkForItems = setInterval(function() {
+        if (jQuery('.add_to_cart_button').length) {
+            updateCart();
+        }
+    }, 500);
+
 });
+
+function updateCart() {
+    jQuery('.add_to_cart_button').click(() => {
+        window.setTimeout(() => {
+            jQuery.post({
+                url:'../wp-admin/admin-ajax.php',
+                datatype: 'JSON',
+                data: {action:'get_wc_cart_info'},
+                success:function(result) {
+                    let parsedResult = JSON.parse(result);
+                    jQuery('.custom-cart').removeClass('hidden');
+                    jQuery('.items').html(parsedResult[0]);
+                    jQuery('.price').html(parsedResult[1]);
+                }
+            });
+        }, 2000);
+    });
+}
